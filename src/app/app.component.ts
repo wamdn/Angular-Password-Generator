c0c9ac0bdadc39a15860f1b5e0886173f403c2e5
wail <wail.madrane@gmail.com>
2022-01-24 20:11:22 +0100

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+  public passLen = 0;
+  public useLetters = false;
+  public useNumbers = false;
+  public useSymbols = false;
+  public password = '';
+
+  public faCopy = faCopy;
+
+  constructor(private clipboard: Clipboard) {}
+
+  copyText(textToCopy: string) {
+    this.clipboard.copy(textToCopy);
+  }
+
+  public onInputLength(e: Event): void {
+    const len = parseInt((<HTMLInputElement>e.target).value, 10);
+    const maxLen = 255;
+    this.passLen = isNaN(len) ? 0 : len > maxLen ? maxLen : len;
+  }
+
+  public onGeneratePassword(): void {
+    const letters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+    const numbers = '0123456789';
+    const symbols = '~`! @#$%^&*()_-+={[}]|\\:;"\'<,>.?/';
+    const charArray: string[] = [];
+    let allCharaters = '';
+
+    if (this.useLetters) allCharaters += letters;
+    if (this.useNumbers) allCharaters += numbers;
+    if (this.useSymbols) allCharaters += symbols;
+
+    for (let i = 0; i < this.passLen; i++) {
+      const charIdx = Math.floor(Math.random() * allCharaters.length);
+      charArray.push(allCharaters[charIdx]);
+    }
+    this.password = charArray.join('');
+  }
 }
